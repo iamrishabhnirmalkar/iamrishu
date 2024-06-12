123341aa3b088c0fad973b5c5fcf824119900dc5
Rishabh Nirmalkar <iamrishabhnirmalkar@gmail.com>
2024-06-12 10:03:24 +0530

    header

---
+++ src/components/layout/MobNav.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useState } from "react";
+import { navpageRoute } from "./config";
+import { RiMenu2Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function MobNav() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  return (
+    <>
+      {/* Navlink for Tablet and Mobile */}
+      <nav className="lg:hidden block py-4 px-3">
+        <header className="flex justify-between items-center">
+          <div
+            className=" bg-custompurple02 p-3 rounded-full relative z-50 cursor-pointer"
+            onClick={toggleMobileMenu}
+          >
+            {mobileMenu ? (
+              <AiOutlineClose size={25} />
+            ) : (
+              <RiMenu2Line size={25} />
+            )}
+          </div>
+          <button>Hire Me</button>
+        </header>
+        <AnimatePresence>
+          {mobileMenu && (
+            <motion.div
+              className="fixed inset-0 flex justify-center items-center z-40"
+              initial={{ clipPath: "circle(30px at 10% 10%)" }}
+              animate={{ clipPath: "circle(150% at 50% 50%)" }}
+              exit={{ clipPath: "circle(30px at 10% 10%)" }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="bg-custompurple02 w-full h-full flex justify-center items-center">
+                <ul className="flex-col text-center space-y-2">
+                  {navpageRoute.map((route) => (
+                    <li
+                      className="uppercase text-xl hover:px-2 rounded hover:bg-custompurple02 transition-all duration-300"
+                      key={route.id}
+                    >
+                      <a href={route.href}>{route.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </nav>
+    </>
+  );
+}
---
+++ src/components/layout/config.ts
@@ -0,0 +1,24 @@
+import { IPageRoute } from "@/@Types/Routes";
+
+export const navpageRoute: IPageRoute[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "project",
+    href: "/project",
+  },
+  {
+    name: "Blogs",
+    href: "/blogs",
+  },
+  {
+    name: "contact",
+    href: "/contact",
+  },
+];
