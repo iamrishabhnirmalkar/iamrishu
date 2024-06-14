34eea518cee8c4b2da386a10751059cee4fe4247
Rishabh Nirmalkar <iamrishabhnirmalkar@gmail.com>
2024-06-14 15:54:10 +0530

    skills section comp

--- src/components/template/landingPage/SkillSection.tsx
+++ src/components/template/landingPage/SkillSection.tsx
@@ -1,20 +1,22 @@
 import Heading from "@/components/ui/Heading/Heading";
 import SkillsBox from "@/components/ui/SkillsBox/SkillsBox";
 import React from "react";
+import { Skillsinfo } from "@/components/template/config";
 
 export default function SkillSection() {
   return (
     <>
       <section className="lg:px-10 px-3">
         <Heading bgheading="Skills" heading="Skills" />
-        <div className="grid grid-cols-3 gap-4 row-span-2">
-          <SkillsBox />
-          <SkillsBox />
-          <SkillsBox />
-
-          <SkillsBox />
-          <SkillsBox />
-          <SkillsBox />
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-4 row-span-2">
+          {Skillsinfo.map((info, index) => (
+            <SkillsBox
+              key={index}
+              heading={info.heading}
+              description={info.description}
+              images={info.images}
+            />
+          ))}
         </div>
       </section>
     </>
--- src/components/ui/SkillsBox/SkillsBox.tsx
+++ src/components/ui/SkillsBox/SkillsBox.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Image from "next/image";
-import img from "../../../../public/svg/01.gif";
+import { IBoxprops } from "@/@Types/comman";
 import { FaArrowRight } from "react-icons/fa6";
 
-export default function SkillsBox() {
+export default function SkillsBox({ heading, description, images }: IBoxprops) {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <div className="w-[22rem] rounded border border-blue-800">
-          <div className="flex">
-            <Image src={img} width={200} height={200} alt="image" />
-            <FaArrowRight size={25} />
+      <div className="flex justify-center items-center ">
+        <div className="w-full h-full rounded glassmorphism px-3 py-5 ">
+          <div className="flex justify-between item-center pb-6 ">
+            <Image
+              src={images}
+              width={50}
+              height={10}
+              className="rounded-full glow-icon"
+              alt="image"
+            />
+
+            <FaArrowRight className="glow-icon" size={25} />
           </div>
-          <h4>Heading</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-            molestias, adipisci amet vitae possimus provident sit tempora quis,
-            omnis rem et illo enim eius asperiores nostrum doloribus illum,
-            consectetur maiores!
-          </p>
+          <div className="flex flex-col space-y-2">
+            <h4 className="text-xl text-custompurple02 font-bold">{heading}</h4>
+            <p className="text-justify">{description}</p>
+          </div>
         </div>
       </div>
     </>
